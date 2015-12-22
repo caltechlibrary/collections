90b8fc71b53b416cd6514784b71e7eab26da77e9
Tommy Keswick <tkeswick@caltech.edu>
2015-12-22 09:35:29 -0800

    Add sub-theme.

---
+++ sites/all/themes/journals_foundation/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function journals_foundation_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function journals_foundation_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function journals_foundation_preprocess_node(&$variables) {
+}
