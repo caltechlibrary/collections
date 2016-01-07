cfa6695ed386ab67a8877ec5c558a3f469715c8a
Tommy Keswick <tkeswick@caltech.edu>
2016-01-07 14:20:36 -0800

    Add linkit_views and patch.
    @see https://www.drupal.org/node/2155463

---
+++ docroot/sites/all/drush/drushrc.php
@@ -0,0 +1,7 @@
+<?php
+
+# Patch file is relative to the Drupal root directory. The example would refer
+# a patch file that is located one directory up from the Drupal root. Define
+# this option to save typing when running Drush commands for your project.
+#$options['patch-file'] = '../patches.make';
+$options['patch-file'] = 'sites/all/drush/patches.make';
