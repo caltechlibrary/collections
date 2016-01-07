370c4623ab515eb90b0c8ed5947d4b1836b35b2c
Tommy Keswick <tkeswick@caltech.edu>
2016-01-07 15:28:41 -0800

    Remove alternate menu.

--- docroot/sites/all/themes/journals_foundation/templates/page.tpl.php
+++ docroot/sites/all/themes/journals_foundation/templates/page.tpl.php
@@ -47,46 +47,6 @@
       <!--/.top-bar -->
     <?php endif; ?>
 
-    <!-- Title, slogan and menu -->
-    <?php if ($alt_header): ?>
-      <section class="row <?php print $alt_header_classes; ?>">
-
-        <?php if ($linked_logo): print $linked_logo; endif; ?>
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name" class="element-invisible">
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-
-        <?php if ($alt_main_menu): ?>
-          <nav id="main-menu" class="navigation" role="navigation">
-            <?php print ($alt_main_menu); ?>
-          </nav> <!-- /#main-menu -->
-        <?php endif; ?>
-
-        <?php if ($alt_secondary_menu): ?>
-          <nav id="secondary-menu" class="navigation" role="navigation">
-            <?php print $alt_secondary_menu; ?>
-          </nav> <!-- /#secondary-menu -->
-        <?php endif; ?>
-
-      </section>
-    <?php endif; ?>
-    <!-- End title, slogan and menu -->
-
     <?php if (!empty($page['header'])): ?>
       <!--.l-header-region -->
       <section class="l-header-region row">
