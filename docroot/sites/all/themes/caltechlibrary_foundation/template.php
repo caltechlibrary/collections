1aaf91a8b60791f388da067da38422756f8986de
Tommy Keswick <tkeswick@caltech.edu>
2016-01-20 16:45:43 -0800

    Add sub-theme.

---
+++ docroot/sites/all/themes/caltechlibrary_foundation/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function caltechlibrary_foundation_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function caltechlibrary_foundation_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function caltechlibrary_foundation_preprocess_node(&$variables) {
+}
---
+++ docroot/sites/all/themes/caltechlibrary_foundation/theme-settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Implements hook_form_FORM_ID_alter().
+ */
+function caltechlibrary_foundation_form_system_theme_settings_alter(&$form, &$form_state) {
+
+}
